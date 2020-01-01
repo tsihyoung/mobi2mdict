1ca0d7c78a095db1dac568e5814fab7017c3a15f
Ziyang <tsihyoung@gmail.com>
2020-01-01 22:32:11 +0800

    1. rewrite all codes into perl, 2. does not deal with <span> now.

--- README.md
+++ README.md
@@ -6,9 +6,8 @@ These scripts can convert *SOME* MOBI dictionaries to Octopus MDict source witho
 Dependencies:
 
 * Perl 5
-* gnu-sed if you are working on MacOS
 
 How to:
 
 1. Unpack MOBI file to html.
-2. Simply run `./mobi2mdict.sh file_got_in_step_1.html`
+2. Simply run `./mobi2mdict.pl file_got_in_step_1.html`
---
+++ mobi2mdict.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/perl
+use List::MoreUtils qw(uniq);
+
+open(FILE,$ARGV[0]) or die "File failed to open: $!";
+my @dict = <FILE>;
+close FILE;
+
+print "Preprocessing HTML format...\n";
+
+foreach (@dict) {
+    $_ =~ s/<idx:entry scriptable="yes">/\n<\/>\n/g;
+    $_ =~ s/<\/idx:entry>//g;
+    $_ =~ s/<idx:orth value="(.*?)">/\1\n/g;
+    $_ =~ s/<\/idx:orth>//g;
+    $_ =~ s/<hr\/>//g;
+    $_ =~ s/<mbp:frameset>//g;
+    $_ =~ s/<\/mbp:frameset>//g;
+    $_ =~ s/<mbp:pagebreak\/>//g;
+    $_ =~ s/<html>//g;
+    $_ =~ s/<\/html>//g;
+    $_ =~ s/<body>//g;
+    $_ =~ s/<\/body>//g;
+}
+$dict[-1] .= "\n<\/>\n";
+
+open(FILE,"+>",$ARGV[0]) or die "File failed to open: $!";
+print FILE @dict;
+close FILE;
+
+open(FILE,$ARGV[0]) or die "File failed to open: $!";
+my @dict = <FILE>;
+
+my @copyright = splice(@dict,0,2);
+open(COPYRIGHT,"+>","copyright.txt") or die "File failed to open: $!";
+print COPYRIGHT $copyright[0];
+close COPYRIGHT;
+
+seek(FILE, 0, 0);
+my $prev = "";
+while (my $line = <FILE>) {
+    $prev =~ s/\n//;
+    if (my @ids = uniq($line =~ /a id="(.*?)"/g)) {
+        for $id (@ids) {
+            print "processing: $id\n";
+            foreach (@dict) {
+                $_ =~ s/<a  href="#$id" >/<a href="entry:\/\/$prev">/g;
+                $_ =~ s/<a id="$id" \/>//g;
+            }
+        }
+    }
+    $prev = $line;
+}
+
+close FILE;
+
+my @inflections = ();
+$prev = "";
+for $line (@dict) {
+    if ($line =~ /<idx:iform name="" value="(.*?)"\/>/) {
+        print "processing: $prev";
+        $prev =~ s/\n//;
+        foreach ($line =~ /<idx:iform name="" value="(.*?)"\/>/g) {
+            if (substr($_, 0, 1) =~ /[^\-(]/ && $_ ne $prev) {
+                push @inflections, "$_\n<div>la flexion de <b><a href=\"entry://$prev\">$prev</b></div>\n</>\n";
+            }
+        }
+        $line =~ s/<idx:infl>(.*)<\/idx:infl>(.*?)<\/div>/\2<\/div><div>\1<\/div>/;
+        $line =~ s/<idx:iform name="" value="(.*?)"\/>/\1 /g;
+    }
+    $prev = $line;
+}
+
+open(FILE,"+>",$ARGV[0]) or die "File failed to open: $!";
+print FILE @dict, @inflections;
+close FILE;
+
+print "All done";
